f7fabb56911a24f88bde05353707498d794a02e0
Colin Harfst <colinharfst@gmail.com>
2020-03-10 20:59:57 -0400

    unstyled home text and resume update

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -5,7 +5,16 @@ export function Home() {
 
   return (
     <div>
-      <h2>Here's some home text</h2>
+      <h2>Somehow you've made it to my website. You probably know me personally or are a recruiter. In either case, I don't have much to show you.</h2>
+      <h3>
+        On this site you'll be able to find my resume, some math research from college, and a collection of projects. Some of these projects are ideas that I've abandoned, while others are an attempt
+        to track my interests.
+      </h3>
+      <h3>On this site you'll be able to find unstyled html. You'll be able to see dynamic content generated with basic front-end and back-end procedures.</h3>
+      <h3>
+        I'd be remiss to not mention the ReactJS + NodeJS tech stack running the site, or the Mongo cloud database that the site is connected to, or the CI/CD pipeline deploying the site using GitLab.
+      </h3>
+      <h3>I'd love to be designing this site with the hopes of it being the next billion dollar idea or with the intention of it landing me an incredible job, but instead I'm designing a blog.</h3>
     </div>
   );
 }
