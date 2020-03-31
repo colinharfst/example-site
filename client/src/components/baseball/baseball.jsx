03ffdbcd6c310db5b1c8b308b90707e64eb3ffed
Colin Harfst <colinharfst@gmail.com>
2020-03-31 12:41:03 -0400

    baseball component styling

--- client/src/components/baseball/baseball.jsx
+++ client/src/components/baseball/baseball.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import Judge from "./judge";
+import "./baseball.scss";
 
 export function Baseball() {
   const [playerId, setPlayerId] = useState("592450");
   const [playerName, setPlayerName] = useState("Judge");
   const [teamName, setTeamName] = useState("Yankees");
   const switchPlayers = () => {
-    console.log("here");
     if (playerName === "Judge") {
       setPlayerId("514888");
-      setPlayerName("Notorious Cheater Jose Altuve");
+      setPlayerName("Notorious Cheater José Altuve");
       setTeamName("Astr*s");
     } else {
       setPlayerId("592450");
@@ -17,7 +17,11 @@ export function Baseball() {
       setTeamName("Yankees");
     }
   };
-  return <Judge playerId={playerId} playerName={playerName} teamName={teamName} changePlayer={switchPlayers} />;
+  return (
+    <div className={playerName === "Judge" ? "baseball yankees-player" : "baseball astros-player"}>
+      <Judge playerId={playerId} playerName={playerName} teamName={teamName} changePlayer={switchPlayers} />
+    </div>
+  );
 }
 
 export default Baseball;
--- client/src/components/baseball/judge.jsx
+++ client/src/components/baseball/judge.jsx
@@ -6,7 +6,7 @@ export class Judge extends React.Component {
   state = { hrCount: null, isGameFinal: null, isGameToday: null, playerPlayed: null, lastHRCount: null, lastHRDate: null, wasHRLastGamePlayed: null };
 
   componentDidMount = async () => {
-    document.title = this.props.playerId === "592450" ? "Aaron Judge Stats" : "Cheater Jose Altuve Stats";
+    document.title = this.props.playerId === "592450" ? "Aaron Judge Stats" : "Cheater José Altuve Stats";
     await this.loadTodaysHRCount(this.props.playerId);
     await this.getPlayer(this.props.playerId);
   };
@@ -14,7 +14,7 @@ export class Judge extends React.Component {
   componentDidUpdate = async prevProps => {
     if (this.props.playerId !== prevProps.playerId) {
       this.setState({ hrCount: null, isGameFinal: null, isGameToday: null, playerPlayed: null, lastHRCount: null, lastHRDate: null, wasHRLastGamePlayed: null });
-      document.title = this.props.playerId === "592450" ? "Aaron Judge Stats" : "Cheater Jose Altuve Stats";
+      document.title = this.props.playerId === "592450" ? "Aaron Judge Stats" : "Cheater José Altuve Stats";
       await this.loadTodaysHRCount(this.props.playerId);
       await this.getPlayer(this.props.playerId);
     }
@@ -36,19 +36,19 @@ export class Judge extends React.Component {
 
   getPlayer = async playerId => {
     const player = await fetch(`/api/player-hr/${playerId}`).then(async resp => await resp.json());
-    this.setState({ lastHRCount: player.lastHRCount, lastHRDate: player.lastHRDate, wasHRLastGamePlayed: player.wasHRLastGamePlayed });
+    this.setState({ lastHRCount: player.lastHRCount, lastHRDate: this.getDateString(player.lastHRDate), wasHRLastGamePlayed: player.wasHRLastGamePlayed });
     console.log("getPlayer", this.state);
   };
 
-  updatePlayerHR = async (playerId, hrCount, hrDate) => {
-    await fetch(`/api/player-hr/${playerId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ lastHRCount: hrCount, lastHRDate: hrDate, wasHRLastGamePlayed: true })
-    });
-  };
+  // updatePlayerHR = async (playerId, hrCount, hrDate) => {
+  //   await fetch(`/api/player-hr/${playerId}`, {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({ lastHRCount: hrCount, lastHRDate: hrDate, wasHRLastGamePlayed: true })
+  //   });
+  // };
 
   // updatePlayerNoHR = async playerId => {
   //   await fetch(`/api/player-hr/${playerId}`, {
@@ -60,23 +60,54 @@ export class Judge extends React.Component {
   //   });
   // };
 
+  getDateString = date => {
+    const d = new Date(date);
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+  };
+
+  renderPlayerQuestion = () => {
+    const { playerId } = this.props;
+    if (playerId === "592450") {
+      return <h1>Did Aaron Judge homer?</h1>;
+    }
+    return <h1>Did José Altuve homer?</h1>;
+  };
+
+  renderNextPlayerButton = () => {
+    const { playerId, changePlayer } = this.props;
+    if (playerId === "592450") {
+      return (
+        <button className="next-player-button" onClick={changePlayer}>
+          <h3>What about notorious cheater José Altuve?</h3>
+          <ArrowRightIcon />
+        </button>
+      );
+    }
+    return (
+      <button className="next-player-button" onClick={changePlayer}>
+        <h3>Try Aaron Judge</h3>
+        <ArrowRightIcon />
+      </button>
+    );
+  };
+
   render() {
     const { playerName, teamName } = this.props;
     const { isGameToday, isGameFinal, playerPlayed, hrCount, wasHRLastGamePlayed, lastHRDate, lastHRCount } = this.state;
 
     if (isGameToday === null || lastHRCount === null) {
-      return <CircularProgress />;
+      return <CircularProgress style={{ margin: "40px", color: "#282c34" }} />;
     }
 
     if (isGameToday) {
       if (hrCount) {
         return (
           <div>
-            <h1>YES</h1>
+            {this.renderPlayerQuestion()}
+            <h1 className="yes-text">YES</h1>
             <h2>{`${playerName} hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""} today!`}</h2>
-            <button onClick={this.props.changePlayer}>
-              <ArrowRightIcon />
-            </button>
+            {this.renderNextPlayerButton()}
           </div>
         );
       }
@@ -84,76 +115,69 @@ export class Judge extends React.Component {
         if (playerPlayed) {
           return (
             <div>
-              <h1>No</h1>
-              <h2>{`${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}`}</h2>
-              <button onClick={this.props.changePlayer}>
-                <ArrowRightIcon />
-              </button>
+              {this.renderPlayerQuestion()}
+              <h1 className="no-text">No</h1>
+              <h2>{`${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
+              {this.renderNextPlayerButton()}
             </div>
           );
         }
         if (wasHRLastGamePlayed) {
           return (
             <div>
-              <h1>YES</h1>
+              {this.renderPlayerQuestion()}
+              <h1 className="yes-text">YES</h1>
               <h2>{`${playerName} didn't play today, but he homered in his last game played on ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
-              <button onClick={this.props.changePlayer}>
-                <ArrowRightIcon />
-              </button>
+              {this.renderNextPlayerButton()}
             </div>
           );
         }
         return (
           <div>
-            <h1>No</h1>
-            <h2>{`${playerName} didn't play today and he hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}`}</h2>
-            <button onClick={this.props.changePlayer}>
-              <ArrowRightIcon />
-            </button>
+            {this.renderPlayerQuestion()}
+            <h1 className="no-text">No</h1>
+            <h2>{`${playerName} didn't play today and he hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
+            {this.renderNextPlayerButton()}
           </div>
         );
       }
       if (wasHRLastGamePlayed) {
         return (
           <div>
-            <h1>YES</h1>
+            {this.renderPlayerQuestion()}
+            <h1 className="yes-text">YES</h1>
             <h2>{`The ${teamName} haven't finished playing yet today, but ${playerName} homered in his last game played on ${lastHRDate}. He hit ${lastHRCount} home run${
               lastHRCount > 1 ? "s" : ""
             }.`}</h2>
-            <button onClick={this.props.changePlayer}>
-              <ArrowRightIcon />
-            </button>
+            {this.renderNextPlayerButton()}
           </div>
         );
       }
       return (
         <div>
-          <h1>No</h1>
-          <h2>{`The ${teamName} haven't finished playing yet today, but ${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}`}</h2>
-          <button onClick={this.props.changePlayer}>
-            <ArrowRightIcon />
-          </button>
+          {this.renderPlayerQuestion()}
+          <h1 className="no-text">No</h1>
+          <h2>{`The ${teamName} haven't finished playing yet today, but ${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
+          {this.renderNextPlayerButton()}
         </div>
       );
     }
     if (wasHRLastGamePlayed) {
       return (
         <div>
-          <h1>YES</h1>
+          {this.renderPlayerQuestion()}
+          <h1 className="yes-text">YES</h1>
           <h2>{`The ${teamName} didn't play today, but ${playerName} homered in his last game played on ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
-          <button onClick={this.props.changePlayer}>
-            <ArrowRightIcon />
-          </button>
+          {this.renderNextPlayerButton()}
         </div>
       );
     }
     return (
       <div>
-        <h1>No</h1>
-        <h2>{`The ${teamName} didn't play today, but ${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}`}</h2>
-        <button onClick={this.props.changePlayer}>
-          <ArrowRightIcon />
-        </button>
+        {this.renderPlayerQuestion()}
+        <h1 className="no-text">No</h1>
+        <h2>{`The ${teamName} didn't play today, but ${playerName} hasn't hit a homer since ${lastHRDate}. He hit ${lastHRCount} home run${lastHRCount > 1 ? "s" : ""}.`}</h2>
+        {this.renderNextPlayerButton()}
       </div>
     );
   }
