d39a1b54d133b9f0a1c24af010f4a758c8fbb902
Colin Harfst <charfst@captechventures.com>
2020-07-19 14:18:10 -0400

    show math research as image only to support Safari mobile browser (for multi-page PDFs) :(

--- client/src/components/math/math.jsx
+++ client/src/components/math/math.jsx
@@ -16,22 +16,22 @@ export function Math() {
 
   return (
     <div className="research-pdf-wrapper">
-      <object
+      {/* <object
         className="research"
         data={research}
         type="application/pdf"
         width="100%"
         // onLoad={console.log("load")}
         // onError={console.log("error")}
-      >
-        <h3>Click the images below to download my research.</h3>
-        <a href={research}>
-          <img src={researchImg1} alt="research page 1" />
-        </a>
-        <a href={research}>
-          <img src={researchImg2} alt="research page 2" />
-        </a>
-      </object>
+      > */}
+      <h3>Click the images below to download my research.</h3>
+      <a href={research}>
+        <img src={researchImg1} alt="research page 1" />
+      </a>
+      <a href={research}>
+        <img src={researchImg2} alt="research page 2" />
+      </a>
+      {/* </object> */}
     </div>
   );
 }
