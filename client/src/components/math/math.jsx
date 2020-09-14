62aa07c7b3f5bfde32cd3c4221e9b6a1d168c4aa
Colin Harfst <charfst@captechventures.com>
2020-09-14 13:36:57 -0400

    minor style changes

--- client/src/components/math/math.jsx
+++ client/src/components/math/math.jsx
@@ -7,33 +7,17 @@ import researchImg2 from "./research-2.png";
 export function Math() {
   document.title = "Math Research";
 
-  // hideWidgets = () => {
-  //   const icons = document.getElementsByClassName("icons")[0];
-  //   const darkModeSwitch = document.getElementsByClassName(".MuiSwitch-root.dark-mode-switch")[0];
-  //   icons.style["z-index"] = -1;
-  //   darkModeSwitch.style["z-index"] = -1;
-  // };
-
   return (
     <div className="research-pdf-wrapper">
-      {/* <object
-        className="research"
-        data={research}
-        type="application/pdf"
-        width="100%"
-        // onLoad={console.log("load")}
-        // onError={console.log("error")}
-      > */}
       <h3>Click the images below to download my research.</h3>
-      <a href={research}>
-        <img src={researchImg1} alt="research page 1" />
-      </a>
-      <a href={research}>
-        <img src={researchImg2} alt="research page 2" />
-      </a>
-      <div style={{ paddingBottom: "60px" }} />
-
-      {/* </object> */}
+      <div style={{ marginBottom: "60px" }}>
+        <a href={research}>
+          <img src={researchImg1} alt="research page 1" />
+        </a>
+        <a href={research}>
+          <img src={researchImg2} alt="research page 2" />
+        </a>
+      </div>
     </div>
   );
 }
--- client/src/components/resume/resume.jsx
+++ client/src/components/resume/resume.jsx
@@ -9,11 +9,12 @@ export function Resume() {
   return (
     <div className="resume-pdf-wrapper">
       <object className="resume" data={resume} type="application/pdf" width="100%">
-        <h3>Click the image below to download my resume.</h3>
-        <a href={resume}>
-          <img src={resumeImg} alt="resume" />
-        </a>
-        <div style={{ paddingBottom: "60px" }} />
+        <div style={{ marginBottom: "60px" }}>
+          <h3>Click the image below to download my resume.</h3>
+          <a href={resume}>
+            <img src={resumeImg} alt="resume" />
+          </a>
+        </div>
       </object>
     </div>
   );
