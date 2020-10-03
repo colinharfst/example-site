b40e770c3041b77e05a87445b7c56c8c61d0786b
Colin Harfst <charfst@captechventures.com>
2020-10-02 23:18:36 -0400

    for my own vanity

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import "./home.scss";
+import self from "./self.png";
+import selfie from "./selfie.png";
 
 export function Home() {
   document.title = "Colin Harfst - Home";
 
   return (
-    <div className="home-text">
-      <h3>
-        Somehow you've made it to my website. You probably know me personally or are a recruiter. In either case,
-        welcome.
-      </h3>
-      <h3>
-        On this site you'll be able to find my resume, some math research from college, and a collection of projects.
-        Most of these projects have been abandoned and now serve as an artifact of something I was once interested in.
-      </h3>
-      <h3>
-        Within this minimalist site there are some signs of a modern full-stack application, built with ReactJS and
-        NodeJS, connected to a MongoDB cloud database, deployed via a GitLab CI/CD pipeline, and hosted with Heroku.
-        Otherwise, this site is mostly underwhelming, unstyled HTML serving as a blog.
-      </h3>
-    </div>
+    <>
+      <img
+        src={Math.floor(Math.random() * 2) ? self : selfie}
+        alt={Math.floor(Math.random() * 2) ? "not a selfie" : "a selfie"}
+        height={250}
+      />
+      <div className="home-text">
+        <h3>
+          Hi! Somehow you've made it to my website. You probably know me personally or are a recruiter. In either case,
+          welcome.
+        </h3>
+        <h3>
+          On this site you'll be able to find my resume and a collection of small projects that I've worked on for my
+          own entertainment.
+        </h3>
+        <h3>
+          Within this minimalist site there are some signs of a modern full-stack application, built with ReactJS and
+          NodeJS, connected to a MongoDB cloud database, deployed via a GitLab CI/CD pipeline, and hosted with Heroku.
+        </h3>
+      </div>
+    </>
   );
 }
 
