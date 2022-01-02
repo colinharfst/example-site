004375514d0fb566b2069aacd946c79e2035877f
Colin Harfst <colinharfst@gmail.com>
2022-01-01 21:48:38 -0500

    Update resume

--- client/src/components/resume/resume.jsx
+++ client/src/components/resume/resume.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./resume.scss";
-import resume from "./resume-2021.pdf";
-import resumeImg from "./resume-2021.png";
+import resume from "./Colin_Harfst_Resume_2022.pdf";
+import resumeImg from "./Colin_Harfst_Resume_2022.png";
 
 export function Resume() {
-  document.title = "Resume";
+	document.title = "Resume";
 
-  return (
-    <div className="resume-pdf-wrapper">
-      <object className="resume" data={resume} type="application/pdf" width="100%">
-        <div style={{ marginBottom: "60px" }}>
-          <h3>Click the image below to download my resume.</h3>
-          <a href={resume}>
-            <img src={resumeImg} alt="resume" />
-          </a>
-        </div>
-      </object>
-    </div>
-  );
+	return (
+		<div className="resume-pdf-wrapper">
+			<object className="resume" data={resume} type="application/pdf" width="100%">
+				<div style={{ marginBottom: "60px" }}>
+					<h3>Click the image below to download my resume.</h3>
+					<a href={resume}>
+						<img src={resumeImg} alt="resume" />
+					</a>
+				</div>
+			</object>
+		</div>
+	);
 }
 
 export default Resume;
