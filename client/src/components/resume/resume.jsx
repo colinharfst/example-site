847c99219c431124b3ab20c914c7cce5ccc41653
Colin Harfst <colinharfst@gmail.com>
2022-01-02 21:32:00 -0500

    Move resume to public folder

--- client/src/components/resume/resume.jsx
+++ client/src/components/resume/resume.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./resume.scss";
-import resume from "./Colin_Harfst_Resume_2022.pdf";
-import resumeImg from "./Colin_Harfst_Resume_2022.png";
 
 export function Resume() {
 	document.title = "Resume";
 
 	return (
 		<div className="resume-pdf-wrapper">
-			<object className="resume" data={resume} type="application/pdf" width="100%">
+			<object className="resume" data="/Colin-Harfst-Resume-2022.pdf" type="application/pdf" width="100%">
 				<div style={{ marginBottom: "60px" }}>
 					<h3>Click the image below to download my resume.</h3>
-					<a href={resume}>
-						<img src={resumeImg} alt="resume" />
+					<a href="/Colin-Harfst-Resume-2022.pdf">
+						<img src="/Colin-Harfst-Resume-2022.png" alt="resume" />
 					</a>
 				</div>
 			</object>
