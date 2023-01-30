1f3f8fb81a55bd5f293c9ca7a757b5d4ca737684
Colin Harfst <colinharfst@gmail.com>
2023-01-29 21:00:22 -0800

    Update resume

--- client/src/components/resume/resume.jsx
+++ client/src/components/resume/resume.jsx
@@ -6,11 +6,11 @@ export function Resume() {
 
 	return (
 		<div className="resume-pdf-wrapper">
-			<object className="resume" data="/Colin-Harfst-Resume-2022.pdf" type="application/pdf" width="100%">
+			<object className="resume" data="/Colin-Harfst-Resume-2023.pdf" type="application/pdf" width="100%">
 				<div style={{ marginBottom: "60px" }}>
 					<h3>Click the image below to download my resume.</h3>
-					<a href="/Colin-Harfst-Resume-2022.pdf">
-						<img src="/Colin-Harfst-Resume-2022.png" alt="resume" />
+					<a href="/Colin-Harfst-Resume-2023.pdf">
+						<img src="/Colin-Harfst-Resume-2023.png" alt="resume" />
 					</a>
 				</div>
 			</object>
